d84d55ba133b2b22737046ff73258dc646a471e3
MurariHarish <harishmeg18@hotmail.com>
2023-11-07 14:40:59 -0500

    project

--- main.tf
+++ main.tf
@@ -1,6 +1,12 @@
 resource "google_storage_bucket" "zillow_data" {
   name                     = "zillow_data_bucket"
+  project                  = "zillow_project"
   location                 = "US"
   force_destroy            = true
   public_access_prevention = "enforced"
 }
+
+# provider "google" {
+#   project = "zillow_project"
+#   region  = "US"
+# }
