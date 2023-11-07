8da426d0823f84affe55d81c4fcc32d9ddd1aa2a
MurariHarish <harishmeg18@hotmail.com>
2023-11-07 14:31:36 -0500

    format

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "google_storage_bucket" "zillow_data" {
-    name                        = "zillow_data_bucket"
-    location                    = "US"
-    force_destroy               = true
-    public_access_prevention    = "enforced"
+  name                     = "zillow_data_bucket"
+  location                 = "US"
+  force_destroy            = true
+  public_access_prevention = "enforced"
 }
